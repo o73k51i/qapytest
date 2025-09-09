a04c38223468e0a4471a40556aa40c2e5adfb467
o73k51i <o73k51i@proton.me>
2025-09-09 11:03:18 +0100

    Add initial module for QAPyTest package

---
+++ qapytest/__init__.py
@@ -0,0 +1,5 @@
+"""QAPyTest is a powerful package for QA specialists built on top of Pytest.
+
+Unlike a simple plugin, it offers a comprehensive system for writing clear tests,
+structuring them with steps, and generating informative interactive HTML reports.
+"""
