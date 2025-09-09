84fc0b141dacd35f80ce03365e48971c0f7243a6
o73k51i <o73k51i@proton.me>
2025-09-09 11:41:24 +0100

    Add core logic and plugin implementation for QAPyTest

---
+++ qapytest/_plugin.py
@@ -0,0 +1,10 @@
+"""QAPyTest core logic and pytest plugin.
+
+This module implements the core logic for the QAPyTest package and
+functions as an internal plugin for Pytest.
+
+It defines custom settings, hooks, and extends the functionality
+of Pytest.
+"""
+
+from __future__ import annotations
