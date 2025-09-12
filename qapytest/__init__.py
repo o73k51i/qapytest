01cfe22289b6951767042b681e110047ddc0e5a3
o73k51i <o73k51i@proton.me>
2025-09-12 21:30:01 +0100

    Refactor public API

--- qapytest/__init__.py
+++ qapytest/__init__.py
@@ -4,10 +4,15 @@ Unlike a simple plugin, it offers a comprehensive system for writing clear tests
 structuring them with steps, and generating informative interactive HTML reports.
 """
 
-from qapytest._external import HttpClient, attach, soft_assert, step
+from qapytest._attach import attach
+from qapytest._http import HttpClient
+from qapytest._soft_assert import soft_assert
+from qapytest._sql import SqlClient
+from qapytest._step import step
 
 __all__ = [
     "HttpClient",
+    "SqlClient",
     "attach",
     "soft_assert",
     "step",
